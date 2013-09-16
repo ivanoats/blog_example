bda4c79b99d45e4d907258884d86a348f1096f7c
Ivan Storck <ivanoats@gmail.com>
2013-09-16 10:29:46 -0700

    scaffolded blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'c5b198dbb73dd26363efa68c7cb17ccea0d7daea594646408c894f235dfed72c8e8f48140846173afbc648d7a2f4095477cf93361e9d4578e97c624f1627e87a'
