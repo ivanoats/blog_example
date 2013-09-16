163aaa78120117d016fa8ea075e4990fa8b1e120
Ivan Storck <ivanoats@gmail.com>
2013-09-16 15:58:11 -0700

    http basic auth

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  http_basic_authenticate_with name: "cookiemonster", password: "secret"
 
   # GET /posts
   # GET /posts.json
